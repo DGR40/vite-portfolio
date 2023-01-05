a71d1dfec54d026a56d2a1f35d5cd9d534394a04
Danny <dgr73@cornell.edu>
2023-01-05 13:14:19 -0500

    t

--- src/routes/resume.jsx
+++ src/routes/resume.jsx
@@ -2,14 +2,9 @@ import PDF from "../assets/dgrResume2023pdf-1.pdf";
 import React, { useState, useEffect } from "react";
 
 export default function Resume() {
-  useEffect(() => hideNavbar());
   return (
     <>
       <iframe src={PDF} style={{ width: "100vw", height: "100vh" }} />
     </>
   );
-}
-
-function hideNavbar() {
-  document.getElementById("navbar").style.display = "none";
 }
